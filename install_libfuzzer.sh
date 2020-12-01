9cf21e2c24fae65cc0f8bc53f541eee9e7008a3b
shimashimashi <n4ag3a2sh1i@gmail.com>
2020-12-02 01:20:24 +0900

    fix

--- install_libfuzzer.sh
+++ install_libfuzzer.sh
@@ -2,7 +2,7 @@
 if [ -e Fuzzer ]; then
   (cd Fuzzer && git pull)
 else
-  git clone https://chromium.googlesource.com/chromium/llvm-project/llvm/lib/Fuzzer
+  git clone https://chromium.googlesource.com/chromium/llvm-project/compiler-rt/lib/fuzzer
 fi
 
 ./Fuzzer/build.sh
